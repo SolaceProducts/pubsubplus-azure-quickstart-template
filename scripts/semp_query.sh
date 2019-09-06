6ffcee38ca72c071770f9538408486b6918f94d1
bczoma <balazs.czoma@solace.com>
2019-09-06 16:17:23 -0400

    semp query fix

--- scripts/semp_query.sh
+++ scripts/semp_query.sh
@@ -37,24 +37,24 @@ shift $((OPTIND-1))
 
 if [[ ${url} = "" || ${name} = "" || ${password} = "" || ${query} = "" ]]; then
   echo "`date` ERROR:${script_name}: url, name, password and query are madatory fields" >&2
-  echo  '{"errorInfo":"missing parameter"}'
+  echo  "{\"errorInfo\":\"missing parameter\"}"
   exit 1
 fi
 if [ `curl --write-out '%{http_code}' --silent --output /dev/null -u ${name}:${password} ${url}` != "200" ] ; then
-  echo  '{"errorInfo":"management host is not responding"}'
+  echo  "{\"errorInfo\":\"management host is not responding\"}"
   exit 1
 fi
 
 query_response=`curl -u ${name}:${password} ${url} -d "${query}"`
 # Validate first char of response is "<", otherwise no hope of being valid xml
 if [[ ${query_response:0:1} != "<" ]] ; then
-  echo  '{"errorInfo":"no valid xml returned"}'
+  echo  "{\"errorInfo\":\"no valid xml returned\"}"
   exit 1
 fi
 query_response_code=`echo $query_response | xmllint -xpath 'string(/rpc-reply/execute-result/@code)' -`
 
 if [[ -z ${query_response_code} && ${query_response_code} != "ok" ]]; then
-  echo  '{"errorInfo":"query failed -${query_response_code}-"}'
+  echo  "{\"errorInfo\":\"query failed -${query_response_code}-\"}"
   exit 1
 fi
 
@@ -62,7 +62,7 @@ fi
 
 if [[ ! -z $value_search ]]; then
   value_result=`echo $query_response | xmllint -xpath "string($value_search)" -`
-  echo  '{"errorInfo":"","valueSearchResult":"${value_result}"}'
+  echo  "{\"errorInfo\":\"\",\"valueSearchResult\":\"${value_result}\"}"
   exit 0
 fi
 
@@ -70,6 +70,6 @@ if [[ ! -z $count_search ]]; then
   count_line=`echo $query_response | xmllint -xpath "$count_search" -`
   count_string=`echo $count_search | cut -d '"' -f 2`
   count_result=`echo ${count_line} | tr "><" "\n" | grep -c ${count_string}`
-  echo  '{"errorInfo":"","countSearchResult":${count_result}}'
+  echo  "{\"errorInfo\":\"\",\"countSearchResult\":${count_result}}"
   exit 0
 fi
